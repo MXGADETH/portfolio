a770e9724fd7aa0038e06dd4bf6c6159b2272638
MXGADETH <thanachotwiratsophon@gmail.com>
2026-06-28 03:04:55 +0700

    Delete api.js

--- api.js
+++
@@ -1,2 +0,0 @@
-const PORTFOLIO_API = "https://script.google.com/macros/s/AKfycbwXF9QpZVeYqqNU38fLnnMti6hSv2zfyMAIyjU7HnJUKSVSQf_JZTUNTq1NRPUkIK7l9A/exec";
-const USERS_API = "https://script.google.com/macros/s/AKfycbyzQDIFVDguPPKMHZiozKjH998A-IrhRgWbDj3RbeW1UZCPbMK6l2nYmHapAkxM3weWpw/exec";
